04d11e37fb54c336381ea3cde5954b03b98033eb
manuelernesto <manuelernesto@outlook.com>
2021-01-22 11:27:26 +0100

    first commit

---
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -0,0 +1,69 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final ItemRepository itemRepository;
+
+    public CartController(UserRepository userRepository, CartRepository cartRepository, ItemRepository itemRepository) {
+        this.userRepository = userRepository;
+        this.cartRepository = cartRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    @PostMapping("/addToCart")
+    public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+        User user = userRepository.findByUsername(request.getUsername());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Optional<Item> item = itemRepository.findById(request.getItemId());
+        if (!item.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Cart cart = user.getCart();
+        IntStream.range(0, request.getQuantity())
+                .forEach(i -> cart.addItem(item.get()));
+        cartRepository.save(cart);
+        return ResponseEntity.ok(cart);
+    }
+
+    @PostMapping("/removeFromCart")
+    public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
+        User user = userRepository.findByUsername(request.getUsername());
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Optional<Item> item = itemRepository.findById(request.getItemId());
+        if (!item.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Cart cart = user.getCart();
+        IntStream.range(0, request.getQuantity())
+                .forEach(i -> cart.removeItem(item.get()));
+        cartRepository.save(cart);
+        return ResponseEntity.ok(cart);
+    }
+
+}
---
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+
+	public OrderController(UserRepository userRepository, OrderRepository orderRepository) {
+		this.userRepository = userRepository;
+		this.orderRepository = orderRepository;
+	}
+
+
+	@PostMapping("/submit/{username}")
+    public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        UserOrder order = UserOrder.createFromCart(user.getCart());
+        orderRepository.save(order);
+        return ResponseEntity.ok(order);
+    }
+
+    @GetMapping("/history/{username}")
+    public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(orderRepository.findByUser(user));
+    }
+}
