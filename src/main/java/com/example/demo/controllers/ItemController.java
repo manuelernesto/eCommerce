9f7f92d4a9e7a1a633146b4f03347b1a1b46150d
manuelernesto <manuelernesto@outlook.com>
2021-01-24 22:43:12 +0100

    add logger to Item and User Controllers

--- src/main/java/com/example/demo/controllers/ItemController.java
+++ src/main/java/com/example/demo/controllers/ItemController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.repositories.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequestMapping("/api/item")
 public class ItemController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     private final ItemRepository itemRepository;
 
     public ItemController(ItemRepository itemRepository) {
@@ -22,16 +26,19 @@ public class ItemController {
 
     @GetMapping
     public ResponseEntity<List<Item>> getItems() {
+        logger.info("Get all items");
         return ResponseEntity.ok(itemRepository.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        logger.info("Get item by id:{}", id);
         return ResponseEntity.of(itemRepository.findById(id));
     }
 
     @GetMapping("/name/{name}")
     public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+        logger.info("Get item by name:{}", name);
         List<Item> items = itemRepository.findByName(name);
         return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
                 : ResponseEntity.ok(items);
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final UserRepository userRepository;
     private final CartRepository cartRepository;
@@ -25,11 +29,13 @@ public class UserController {
 
     @GetMapping("/id/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
+        logger.info("Get user by Id:{}", id);
         return ResponseEntity.of(userRepository.findById(id));
     }
 
     @GetMapping("/{username}")
     public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        logger.info("Get user by username:{}", username);
         User user = userRepository.findByUsername(username);
         return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
     }
@@ -47,6 +53,7 @@ public class UserController {
         }
         user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
         userRepository.save(user);
+        logger.info("Create new user with username :{}", user.getUsername());
         return ResponseEntity.ok(user);
     }
 
